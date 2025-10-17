0b9b32c9cb68a49221a35ae7497b20d0fb6922b5
Awatef Ben Romdhane <awatefbr@hotmail.com>
2025-10-17 14:30:00 +0000

    main.tf added

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+ backend "azurerm" {
+ resource_group_name = "tfstate-rg"
+ storage_account_name = "tfstateawa123"
+ container_name = "tfstate"
+ key = "rg-demo/terraform.tfstate"
+ use_azuread_auth = true
+ }
+ required_providers {
+ azurerm = {
+ source = "hashicorp/azurerm"
+ version = "~> 3.100"
+ }
+ }
+}
+provider "azurerm" {
+ features {}
+}
+resource "azurerm_resource_group" "rg" {
+ name = "rg-demo"
+ location = "Switzerland North"
+}
