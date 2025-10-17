f27609973c152c489a5256793c77ba14afb0fde6
Sarah Belaid <belaidsarah2004@gmail.com>
2025-10-17 14:55:26 +0000

    Modif RG name pour collaboration

--- main.tf
+++ main.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
  features {}
 }
 resource "azurerm_resource_group" "rg" {
- name = "rg-demo"
- location = "Switzerland North"
+ name = "rg-collab-demo"
+ location = "France Central"
 }
